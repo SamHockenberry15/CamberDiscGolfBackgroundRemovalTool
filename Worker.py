d67dc51c53cfa5bdc450575e15d2914abd1c8b5e
Sam Hockenberry <samhockenberry15@gmail.com>
2023-01-12 21:17:09 -0500

    Removes need for model and fixes Houghs Circles alg

--- Worker.py
+++ Worker.py
@@ -1,6 +1,6 @@
+from PIL import Image, ImageDraw
 from PyQt5.QtCore import QObject, pyqtSignal
-from rembg import remove
-from PIL import Image
+# from rembg import remove
 from rembg.session_simple import SimpleSession
 
 import numpy as np
@@ -36,17 +36,17 @@ class Worker(QObject):
                 input = Image.open(img)
                 splitDir = img.split('\\')
                 name = splitDir[-1]
-                output = remove(input, alpha_matting=True, session=self.session)
-                transP = output.rotate(270)
-                self.tempTransparentBackgroundPictures.append(transP)
+                # output = remove(input, alpha_matting=True, session=self.session)
+                # transP = output.rotate(270)
+                # self.tempTransparentBackgroundPictures.append(transP)
+                #
+                # output.load()
+                # input_again_width, input_again_height = input.size
+                # whiteBackground = Image.new("RGB", (input_again_width + 50, input_again_height + 50), (255, 255, 255))
+                # whiteBackground.paste(input, mask=input.split()[3])
+                # finalWhiteBackground = whiteBackground.rotate(270, fillcolor=(255, 255, 255))
 
-                output.load()
-                input_again_width, input_again_height = output.size
-                whiteBackground = Image.new("RGB", (input_again_width + 50, input_again_height + 50), (255, 255, 255))
-                whiteBackground.paste(output, mask=output.split()[3])
-                finalWhiteBackground = whiteBackground.rotate(270, fillcolor=(255, 255, 255))
-
-                self.tempWhiteBackgroundPictures.append(finalWhiteBackground)
+                self.tempWhiteBackgroundPictures.append(input)
                 self.imageNames.append(name)
                 self.progress.emit(self.pbNum)
 
@@ -60,11 +60,24 @@ class Worker(QObject):
             # Convert RGB to BGR
             curr = open_cv_image[:, :, ::-1].copy()
 
-            imgray = cv.cvtColor(curr, cv.COLOR_BGR2GRAY)
+            scale_percent = 10
+
+            scaled_width = curr.shape[1] * scale_percent / 100
+            scaled_height = curr.shape[0] * scale_percent / 100
+            scaled_width_int = int(scaled_width)
+            scaled_height_int = int(scaled_height)
+            dim = (scaled_width_int, scaled_height_int)
+
+            # resize image
+            resized = cv.resize(curr, dim, interpolation=cv.INTER_AREA)
+
+
+
+            imgray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
             gray_blurred = cv.blur(imgray, (3, 3))
             detected_circles = cv.HoughCircles(gray_blurred,
-                                                cv.HOUGH_GRADIENT, 1, 20, param1=50,
-                                                param2=30, minRadius=350, maxRadius=0)
+                                                cv.HOUGH_GRADIENT, 1, 85, param1=50,
+                                                param2=30, minRadius=70, maxRadius=0)
 
             if detected_circles is not None:
                 # Convert the circle parameters a, b and r to integers.
@@ -73,13 +86,38 @@ class Worker(QObject):
 
                 a, b, r = pt[0], pt[1], pt[2]
 
+                a = a * 10
+                b = b * 10
+                r = r * 9.85
+
                 minX = abs(a-r)
                 maxX = r+a
-                minY = abs(b-r)
-                maxY = r+b
+                minY = abs(b-r+30)
+                maxY = r+b+30
 
-                newWhitePic = open_cv_image[minY:maxY, minX:maxX, :]
-                self.saveFile(Image.fromarray(newWhitePic),num)
+                height = open_cv_image.shape[0]
+                width = open_cv_image.shape[1]
+                lum_img = Image.new("L", [width, height], 0)
+
+                draw = ImageDraw.Draw(lum_img)
+                draw.pieslice([(minX, minY), (maxX, maxY)], 0, 360,
+                              fill=255, outline="white")
+                img_arr = np.array(open_cv_image)
+                lum_img_arr = np.array(lum_img)
+                final_img_arr = np.dstack((img_arr, lum_img_arr))
+
+                intMinX = int(minX)
+                intMaxX = int(maxX)
+                intMinY = int(minY)
+                intMaxY = int(maxY)
+
+                final_img_arr = final_img_arr[intMinY:intMaxY, intMinX:intMaxX, :]
+                fin_img = Image.fromarray(final_img_arr)
+                fin_img = fin_img.rotate(270, fillcolor=(255, 255, 255))
+                whiteBackground = Image.new("RGB", (int(maxX-minX), int(maxY-minY)), (255, 255, 255))
+                whiteBackground.paste(fin_img,box=None, mask=fin_img.split()[3])
+                self.saveFile(whiteBackground,num)
+
 
 
     def saveFile(self, img, imgNum):
