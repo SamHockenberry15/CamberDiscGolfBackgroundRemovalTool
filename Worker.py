0fc616ed9b62b4b318b3272d05d998a4fba68600
SamHockenberry <samhockenberry15@gmail.com>
2025-01-18 20:39:26 -0500

    Changes Alg to ALT and fine tunes alg. More work still needed

--- Worker.py
+++ Worker.py
@@ -9,7 +9,8 @@ class Worker(QObject):
     uiStatus = pyqtSignal(bool)
     finished = pyqtSignal()
     #Scaled percentage for hough circle image processing
-    scale_percent = 10
+    scale = .3
+    minRadius = 0
 
     def __init__(self, inputFiles, outputDir, transparent):
         super(Worker, self).__init__()
@@ -31,21 +32,40 @@ class Worker(QObject):
         if len(self.inputFiles) != 0 and len(self.outputDir) != 0:
             for img in self.inputFiles:
                 input = cv.imread(img,cv.IMREAD_UNCHANGED)
-                input = cv.cvtColor(input, cv.COLOR_BGR2RGB)
+                input = cv.resize(input,(0,0),fx = self.scale, fy = self.scale)
+                self.minRadius = int(int(input.shape[0]) * (3/8))
+
+                # cv.imshow("output", input)
+                # cv.waitKey(0)
+                input_hsv = cv.cvtColor(input, cv.COLOR_BGR2HSV)
                 splitDir = img.split('\\')
                 name = splitDir[-1]
-                open_cv_image = np.array(input)
+                open_cv_image = np.array(input_hsv)
                 # Convert RGB to BGR
                 curr = open_cv_image[:, :, ::-1].copy()
 
-                dim = self.getScaledDim(curr.shape[0],curr.shape[1],self.scale_percent)
+                # dim = self.getScaledDim(curr.shape[0],curr.shape[1],self.scale_percent)
 
-                detected_circles = self.detectCircles(curr, dim)
+                detected_circles = self.detectCircles(curr)
+                circles = detected_circles
 
                 if detected_circles is not None:
                     # Convert the circle parameters a, b and r to integers.
                     detected_circles = np.uint16(np.around(detected_circles))
                     pt = detected_circles[0, 0]
+
+                    # convert the (x, y) coordinates and radius of the circles to integers
+                    # DEBUG to see images and circles
+                    # detected_circles = np.round(detected_circles[0, :]).astype("int")
+                    # # loop over the (x, y) coordinates and radius of the circles
+                    # for (x, y, r) in detected_circles:
+                    #     # draw the circle in the output image, then draw a rectangle
+                    #     # corresponding to the center of the circle
+                    #     cv.circle(input, (x, y), r, (0, 255, 0), 4)
+                    #     cv.rectangle(input, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+                    # # show the output image
+                    # cv.imshow("output", np.hstack([input_hsv, input]))
+                    # cv.waitKey(0)
 
                     minX, maxX, minY, maxY = self.getCoordinates(pt)
 
@@ -53,12 +73,18 @@ class Worker(QObject):
                     original_width = open_cv_image.shape[1]
                     lum_img = Image.new("L", [original_width, original_height], 0)
 
-                    draw = ImageDraw.Draw(lum_img)
-                    draw.pieslice([(minX, minY), (maxX, maxY)], 0, 360,
+                    try:
+                        draw = ImageDraw.Draw(lum_img)
+                        draw.pieslice([(minX, minY), (maxX, maxY)], 0, 360,
                                   fill=255, outline="white")
+                    except:
+                        print("error now -->   " + img)
                     img_arr = np.array(open_cv_image)
+                    img_arr = cv.cvtColor(img_arr, cv.COLOR_HSV2RGB)
                     lum_img_arr = np.array(lum_img)
                     final_img_arr = np.dstack((img_arr, lum_img_arr))
+
+
 
                     intMinX = int(minX)
                     intMaxX = int(maxX)
@@ -84,26 +110,31 @@ class Worker(QObject):
         scaled_height_int = int(scaled_height)
         return scaled_width_int, scaled_height_int
 
-    def detectCircles(self, img, dim):
-        resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-        imgray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
+    def detectCircles(self, img):
+        imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         gray_blurred = cv.blur(imgray, (3, 3))
+        # cv.imshow("output", gray_blurred)
+        # cv.waitKey(0)
+        # return cv.HoughCircles(gray_blurred,
+        #                        cv.HOUGH_GRADIENT, 1.2, 85, param1=50,
+        #                        param2=60, minRadius=355, maxRadius=400)
         return cv.HoughCircles(gray_blurred,
-                                           cv.HOUGH_GRADIENT, 1, 85, param1=50,
-                                           param2=30, minRadius=72, maxRadius=0)
+                                           cv.HOUGH_GRADIENT_ALT, 1.5, self.minRadius-25, param1=300,
+                                           param2=.9, minRadius=self.minRadius, maxRadius=int(self.minRadius+45))
 
     def getCoordinates(self, point):
-        a, b, r = point[0], point[1], point[2]
+        #radius +1 to account for circle rim width
+        a, b, r = point[0], point[1], point[2]+1
         #Required to remove approximate error that comes from changing double -> int
-        y_offset = 30
+        # y_offset = 30
         #Required to shrink circle to edges of disc
         radius_offset = 0.15
 
-        a = a * self.scale_percent
-        b = b * self.scale_percent
-        r = r * (self.scale_percent - radius_offset)
+        # a = a * self.scale_percent
+        # b = b * self.scale_percent
+        # r = r * (self.scale_percent - radius_offset)
 
-        return abs(a - r), r + a, abs(b - r + y_offset), r + b + y_offset
+        return abs(a - r), r + a, abs(b - r), r + b
 
 
     def saveFile(self, img, nameWithExt):
